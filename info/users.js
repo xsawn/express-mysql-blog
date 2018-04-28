081ae2073a33ddfafd8131d10f69dba86b74c6b8
xxShawn <275503088@qq.com>
2018-04-29 00:21:59 +0800

    使用服务端缓存实现简单的注册，登陆，退出，用户信息

--- info/users.js
+++ info/users.js
@@ -1,6 +1,14 @@
-module.exports = [
-					{
-						name: 'shawn', 
-						age: 18
-					}
-				]
+
+let DB = {};
+DB.users = [];
+DB.exist = function(account) {
+	console.log('用户是否存在：',DB.users)
+	return DB.users.some(item=> item && item.account  === account)
+};
+DB.insert = function(user) {
+	DB.users.push(user)
+};
+DB.delete = function(account){};
+DB.update = function(account, option) {};
+
+module.exports = DB;
--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var DB = require('../info/users');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'blog' });
 });
 
+/* 登陆 */
 router.get('/login', function(req, res, next) {
   res.render('login', { title: 'login' });
 });
 
 router.post('/login', function(req, res, next) {
-	let params = req.body; console.log(req.body)
-	if(params.account === 'shawn') {
+	let params = req.body; console.log('请求体：', req.body)
+	if(DB.exist(params.account)){
 		console.log('登陆成功，欢迎', params.account);
 		let loginInfo = {
 			isLogin: true,
@@ -22,12 +24,13 @@ router.post('/login', function(req, res, next) {
 		let resData = {
 			success: true,
 		}
-		res.cookie('account', 'shawn', {signed: true})
+		res.cookie('account', params.account, {signed: true})
 		res.send(resData)
+		// res.redirect('/user')
 	} else {
 		res.send({
 			success: false,
-			message: 'not shawn'
+			message: 'not regist'
 		})
 	}
 
@@ -35,6 +38,7 @@ router.post('/login', function(req, res, next) {
   // res.render('login', { title: 'login' });
 });
 
+/* userInfo */
 router.get('/user', function(req, res, next) {
 	if(req.session.loginInfo) {
 		res.render('user')
@@ -56,12 +60,32 @@ router.get('/userinfo', function(req, res, next) {
 	}
 });
 
-// 退出
+/* 退出 */
 router.get('/logout', function(req, res, next){
 	req.session.user = null;
 	// res.redirect('/')
 	res.send({success: true})
 })
+
+/* 注册 */
+router.get('/regist', function(req, res, next) {
+  res.render('regist', { title: 'regist' });
+});
+
+router.post('/regist', function(req, res, next) {
+	let params = req.body; console.log(req.body)
+	if(DB.exist(params.account)) {
+		let resData = {
+			success: false,
+			message: 'user exist!',
+		}
+		res.send(resData)
+	} else {
+		DB.insert({account: params.account})
+		// res.redirect('/login')
+		res.send({success: true})
+	}
+});
 
 
 
